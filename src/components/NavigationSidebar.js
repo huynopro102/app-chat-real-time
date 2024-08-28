e4096cdcee063d847b12df58e1f3805f7c015433
tuan huy <100185063+huynopro102@users.noreply.github.com>
2024-08-28 22:54:00 +0700

    done NavigationSidebar

--- src/components/NavigationSidebar.js
+++ src/components/NavigationSidebar.js
@@ -7,26 +7,39 @@ import logoImage from "../assets/images/logo.jpg";
 function NavigationSidebar() {
   // Tạo một state để lưu trữ màu sắc của từng biểu tượng
   const [hoveredIcon, setHoveredIcon] = useState(null);
+  const [isActive, setIsActive] = useState(false); // State để theo dõi class 'active'
 
+  const handleOnClick = () => {
+    setIsActive(!isActive); // Đảo ngược giá trị của isActive khi click
+  };
   return (
-    <div className="siderbar">
+    <div className={`siderbar ${isActive ? "active" : ""}`}> {/* Thêm class 'active' vào 'siderbar' nếu isActive là true */}
       <div className="logo_content">
         <div className="logo">
           <box-icon
-            size="md"
+            size="lg"
             color="#fff"
             type="logo"
             name="discord-alt"
             id="logoIndividual"
           ></box-icon>
           <div className="logo_name">CodingLab</div>
-          <box-icon color="#fff" name="menu" size="sm" id="btn"></box-icon>
+          <box-icon
+            color="#fff"
+            name="menu"
+            size="sm"
+            id="btn"
+            onClick={handleOnClick}
+          ></box-icon>
         </div>
       </div>
       <ul className="nav_list">
         <li>
           <a>
-            <box-icon color="#fff" id="iconSearch" name="search"></box-icon>
+            <box-icon color="#fff" id="iconSearch" name="search"
+            className={isActive ? "active":""}
+            onClick={handleOnClick}
+            ></box-icon>
             <input
               style={{ width: "100%" }}
               type="text"
@@ -44,9 +57,11 @@ function NavigationSidebar() {
             <box-icon
               color={hoveredIcon === "dashboard" ? "#1d1b31" : "#fff"}
               name="grid-alt"
+              size="md"
             ></box-icon>
             <span className="links_name">Dashboard</span>
           </a>
+          <span className="tooltip">layout</span>
         </li>
         <li>
           <a
@@ -57,9 +72,12 @@ function NavigationSidebar() {
             <box-icon
               color={hoveredIcon === "profile" ? "#1d1b31" : "#fff"}
               name="user"
+              size="md"
+              className="icon_ul_li"
             ></box-icon>
             <span className="links_name">Profile</span>
           </a>
+          <span className="tooltip">layout</span>
         </li>
         <li>
           <a
@@ -70,10 +88,12 @@ function NavigationSidebar() {
             <box-icon
               color={hoveredIcon === "chat" ? "#1d1b31" : "#fff"}
               name="chat"
+              size="md"
+               className="icon_ul_li"
             ></box-icon>
             <span className="links_name">RoomChat</span>
           </a>
-          <span className="ltooltip">layout</span>
+          <span className="tooltip">layout</span>
         </li>
         <li>
           <a
@@ -84,9 +104,11 @@ function NavigationSidebar() {
             <box-icon
               color={hoveredIcon === "others" ? "#1d1b31" : "#fff"}
               name="border-all"
+              size="md"
             ></box-icon>
             <span className="links_name">Orthers</span>
           </a>
+          <span className="tooltip">layout</span>
         </li>
         <li>
           <a
@@ -97,13 +119,15 @@ function NavigationSidebar() {
             <box-icon
               color={hoveredIcon === "settings" ? "#1d1b31" : "#fff"}
               name="cog"
+              size="md"
             ></box-icon>
             <span className="links_name">Setting</span>
           </a>
+          <span className="tooltip">layout</span>
         </li>
       </ul>
       <div className="profile_content">
-          <img src={logoImage} alt="Profile Logo" />      
+        <img src={logoImage} alt="Profile Logo" />
         <div className="job_name">
           <div className="name">nguyen van d</div>
           <div className="job">java backend</div>
@@ -111,7 +135,6 @@ function NavigationSidebar() {
         <box-icon color="#fff" name="log-out" id="log_out"></box-icon>
       </div>
     </div>
-    
   );
 }
 
